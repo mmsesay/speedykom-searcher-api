afdeb9b69bd0dff3b6221ed1e9feb498d71d9ff0
maej <muhammadsesay8@gmail.com>
2023-03-08 18:01:17 +0000

    Assert: hello page is returing the right response

--- project/app/routes.py
+++ project/app/routes.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from app import app
 from app.fetcher import get_data_from_health_gov
 
@@ -9,3 +10,15 @@ def search_keyword(keyword):
     :return : the response from the invoked function
     """
     return get_data_from_health_gov(keyword)
+
+@app.route("/api/v1/hello", methods=['GET'])
+def index():
+    """
+    Fetches data from health gov health finder
+    :params keyword: this is the keyword to query
+    :return : the response from the invoked function
+    """
+    return jsonify({
+        "status": 200,
+        "data": "Hello"
+    })
--- project/tests/test_main.py
+++ project/tests/test_main.py
@@ -1,40 +1,34 @@
 import os
-import tempfile
-import pytest
+import json
 import unittest
 
 from app import app, db
 
 
-# @pytest.fixture
-# def client():
-#     db_fd, app.config['DATABASE'] = 'sqlite:///users.db' # tempfile.mkstemp()
-#     app.config['TESTING'] = True
-
-#     with app.test_client() as client:
-#         with app.app_context():
-#             app.init_db()
-#         yield client
-
-#     os.close(db_fd)
-#     os.unlink(app.config['DATABASE'])
-
 class StartupTest(unittest.TestCase):
     # executed prior to each test
     def setUp(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-            os.path.join(app.root_path, 'user.db')
-        self.app = app.test_client()
-        db.create_all()
+        with app.app_context():
+            app.config['TESTING'] = True
+            app.config['WTF_CSRF_ENABLED'] = False
+            app.config['DEBUG'] = False
+            app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'users.db')
+            self.client = app.test_client()
+            db.create_all()
  
  
     # executed after each test
     def tearDown(self):
-        db.drop_all()
+        with app.app_context():
+            db.drop_all()
+        # pass
 
+    def test_hello_page(self):
+        """Validate empty user login fields"""
+        response = self.client.get("/api/v1/hello")
+        jsondata = json.loads(response.get_data(as_text=True))
+        self.assertEquals('Hello', jsondata['data'])
+        # self.assertIn(b'H', response.data)
 
 if __name__ == "__main__":
     unittest.main()
